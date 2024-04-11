d5cfb74c3ddae241650423537c61afe207ee209e
root <root@ip-172-31-18-62.ap-southeast-1.compute.internal>
2024-04-11 07:52:25 +0000

    deploy

---
+++ precache-manifest.52589c49049ed5426f20f64945d15b94.js
@@ -0,0 +1,690 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb6fea73991cc41633515762fd7236e",
+    "url": "/download.png"
+  },
+  {
+    "revision": "22a1385cb2de8dbdacfa702376553e5d",
+    "url": "/download1.html"
+  },
+  {
+    "revision": "75f6e9a0019cdd5252d2925b4a9acd52",
+    "url": "/icon-default.png"
+  },
+  {
+    "revision": "4ebadc7855a4dbc4f3f80e5e77643c32",
+    "url": "/icon-inscription-default.png"
+  },
+  {
+    "revision": "b33239750b3e0b55c537eb90d6f5bbea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a96d0ba187899d2912fbefbd0b052bb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "07e48bde07b85d28d4fe",
+    "url": "/static/css/Alert.798543c0.css"
+  },
+  {
+    "revision": "b52b71e51b11566beaf6",
+    "url": "/static/css/Balance.f27c4a01.css"
+  },
+  {
+    "revision": "39bc94d0c895eb0ded08",
+    "url": "/static/css/Bownload.88164c58.css"
+  },
+  {
+    "revision": "7b790b4558079e14da39",
+    "url": "/static/css/Brc20.175f2efb.css"
+  },
+  {
+    "revision": "d978d1aa39d249fae0d7",
+    "url": "/static/css/CVAirdrop.0ae92564.css"
+  },
+  {
+    "revision": "3ac38eb5c145ad44d2d2",
+    "url": "/static/css/Chat.254e5b00.css"
+  },
+  {
+    "revision": "262e98f07d6b0e263186",
+    "url": "/static/css/ChatBanned.dc6b0893.css"
+  },
+  {
+    "revision": "33c24fe6be133653bbe1",
+    "url": "/static/css/ChatBurn.81c3a972.css"
+  },
+  {
+    "revision": "316a1bed8de36f9aca46",
+    "url": "/static/css/ChatIndex.4cf49718.css"
+  },
+  {
+    "revision": "655ec0028b7850610e57",
+    "url": "/static/css/ChatNewPayment.e2eaeb73.css"
+  },
+  {
+    "revision": "793de407545e1771ae57",
+    "url": "/static/css/ChatOfficial.9f960e54.css"
+  },
+  {
+    "revision": "3648799da4fb7f9da9e6",
+    "url": "/static/css/ChatPenalty.cd21d9d4.css"
+  },
+  {
+    "revision": "5cad4fca0cc4e39c4cf3",
+    "url": "/static/css/ChatSet.88ec35dc.css"
+  },
+  {
+    "revision": "8f9ca040af07a3276692",
+    "url": "/static/css/ChatTop.e57789bc.css"
+  },
+  {
+    "revision": "dd923aee734b2012ce61",
+    "url": "/static/css/Check.db5dc583.css"
+  },
+  {
+    "revision": "3d9321b23ef981a38780",
+    "url": "/static/css/Evm.dd10dc97.css"
+  },
+  {
+    "revision": "1df9649f674596c03829",
+    "url": "/static/css/Fav.27ca1d37.css"
+  },
+  {
+    "revision": "93138e729fdd36831deb",
+    "url": "/static/css/FavoriteManage.be04976c.css"
+  },
+  {
+    "revision": "e91dfcfaf0796bc8608c",
+    "url": "/static/css/Gas.26aee0df.css"
+  },
+  {
+    "revision": "e70fd56b9f864cd8d938",
+    "url": "/static/css/Home.8d592c52.css"
+  },
+  {
+    "revision": "611b850ca3689289dbcf",
+    "url": "/static/css/Inscription.c3ac2e4b.css"
+  },
+  {
+    "revision": "bf54b9f57e6b9deb0841",
+    "url": "/static/css/Invitation.2d89d402.css"
+  },
+  {
+    "revision": "867967411fb65a9c0cec",
+    "url": "/static/css/Login.b99c9631.css"
+  },
+  {
+    "revision": "b9ae688bdf735c50f5bd",
+    "url": "/static/css/Market.57742bd5.css"
+  },
+  {
+    "revision": "98cb561f54393446bcac",
+    "url": "/static/css/Me.30a1de3a.css"
+  },
+  {
+    "revision": "df1da639c559c2f919df",
+    "url": "/static/css/MyChats.541d7202.css"
+  },
+  {
+    "revision": "3aa6b54832b1ca328c44",
+    "url": "/static/css/NFT.9deb2a31.css"
+  },
+  {
+    "revision": "e022b527b0eac87a7463",
+    "url": "/static/css/Notice.74f29708.css"
+  },
+  {
+    "revision": "9bda8d1587288116e535",
+    "url": "/static/css/OfficialVerification.3be2b2a1.css"
+  },
+  {
+    "revision": "bd575f00fb25a04458e4",
+    "url": "/static/css/Plate.bd4570f2.css"
+  },
+  {
+    "revision": "feb1b85e5fb3677a3e1f",
+    "url": "/static/css/Pool.01d22146.css"
+  },
+  {
+    "revision": "9d1ebffcddfca75403f8",
+    "url": "/static/css/RedCreate.8ed141c4.css"
+  },
+  {
+    "revision": "e5aec7338f21f54ad68e",
+    "url": "/static/css/RedRecord.07952493.css"
+  },
+  {
+    "revision": "5a072a5544047d4b5f6f",
+    "url": "/static/css/ShareLink.4d862330.css"
+  },
+  {
+    "revision": "3f4088c1329afbab1188",
+    "url": "/static/css/Token.2ada38dc.css"
+  },
+  {
+    "revision": "e84ce09d4cbdc4ed824a",
+    "url": "/static/css/UserInfo.c5e24d97.css"
+  },
+  {
+    "revision": "fb7ca84cc0baa736724b",
+    "url": "/static/css/WhaleTracking.988abfd6.css"
+  },
+  {
+    "revision": "d6ec1c96d896ebc4cb4a",
+    "url": "/static/css/app.024d95fb.css"
+  },
+  {
+    "revision": "0d5ae7fff73b2b4dcbe2",
+    "url": "/static/css/chunk-commons.964244ad.css"
+  },
+  {
+    "revision": "0abf3bdbf2b76ab8d0bd",
+    "url": "/static/css/chunk-libs.c2e177ab.css"
+  },
+  {
+    "revision": "d123d9b2bc87788efb2f",
+    "url": "/static/css/chunk-vant.cdb21971.css"
+  },
+  {
+    "revision": "092cbdafbaa5d88adbad",
+    "url": "/static/css/vendors~Brc20~Chat~ChatTop~NFT~Token.c824b6e7.css"
+  },
+  {
+    "revision": "aae84b07c8e98e894e02",
+    "url": "/static/css/vendors~Brc20~NFT.f0d08b16.css"
+  },
+  {
+    "revision": "197f5743c21a1e6c4f1f",
+    "url": "/static/css/vendors~Chat~ChatTop.52be4699.css"
+  },
+  {
+    "revision": "217d86d16d2e2173ea7df9a7e40fcde9",
+    "url": "/static/fonts/iconfont.217d86d1.woff"
+  },
+  {
+    "revision": "39d6b6f83c3c768a7b89e0bda07732b0",
+    "url": "/static/fonts/iconfont.39d6b6f8.eot"
+  },
+  {
+    "revision": "90453b1c66e12f4da95fc366af9de86e",
+    "url": "/static/fonts/iconfont.90453b1c.ttf"
+  },
+  {
+    "revision": "f36b5c32198e807810334f9d4754fb2c",
+    "url": "/static/fonts/iconfont.f36b5c32.woff2"
+  },
+  {
+    "revision": "0415cf4ebd3976a0db17e2fa1e9812f6",
+    "url": "/static/fonts/index.0415cf4e.eot"
+  },
+  {
+    "revision": "87ee1a70c3af3cb1a4d8de1d96568fa0",
+    "url": "/static/fonts/index.87ee1a70.woff"
+  },
+  {
+    "revision": "abed1cb7ce7794b656c39076aaca6970",
+    "url": "/static/fonts/index.abed1cb7.woff2"
+  },
+  {
+    "revision": "de94a85c73334510b8645762a276917d",
+    "url": "/static/fonts/index.de94a85c.ttf"
+  },
+  {
+    "revision": "c2cea02547f4912de1c56978d638469c",
+    "url": "/static/img/1.c2cea025.png"
+  },
+  {
+    "revision": "a0c7411246a0fe27ced4b0215df7ff51",
+    "url": "/static/img/2.a0c74112.png"
+  },
+  {
+    "revision": "980df93c752c8ce347121c1adaaaef89",
+    "url": "/static/img/3.980df93c.png"
+  },
+  {
+    "revision": "7cc0cbd46c45de91211d743569cd5a45",
+    "url": "/static/img/TP@2x.7cc0cbd4.png"
+  },
+  {
+    "revision": "46e8589598b2f5d587eed2abe9ac0ded",
+    "url": "/static/img/Tw@2x.46e85895.png"
+  },
+  {
+    "revision": "bc894cd18298e7899e7e01dbdf4010e3",
+    "url": "/static/img/alert-alert-bg.bc894cd1.png"
+  },
+  {
+    "revision": "f3f7dc719220eefb7f2f11427fed9112",
+    "url": "/static/img/alert-hot-bg.f3f7dc71.png"
+  },
+  {
+    "revision": "1d98f3646bbfe1c1aac49fdb336ef9cf",
+    "url": "/static/img/audit-bg.1d98f364.svg"
+  },
+  {
+    "revision": "067f81212027ae03f92f6a2e6798ab3a",
+    "url": "/static/img/ave-empty.067f8121.png"
+  },
+  {
+    "revision": "46ab30c8ea5101d664a11afd5f11a6ac",
+    "url": "/static/img/ave-white.46ab30c8.png"
+  },
+  {
+    "revision": "84b0bff8c4ed86cdb154d9f52c5ab80f",
+    "url": "/static/img/aveSniperBot.84b0bff8.png"
+  },
+  {
+    "revision": "69cd8fc36a91151f49d9524dcd08408c",
+    "url": "/static/img/bg-bsc.69cd8fc3.svg"
+  },
+  {
+    "revision": "ee7fe868586c9707ce595c8892a161a4",
+    "url": "/static/img/bg-me-head.ee7fe868.svg"
+  },
+  {
+    "revision": "03efdf33f51a936967dc12d54d0ed39e",
+    "url": "/static/img/binance.03efdf33.png"
+  },
+  {
+    "revision": "c373f8deda8c5e9714b704a9f0f25626",
+    "url": "/static/img/browser.c373f8de.svg"
+  },
+  {
+    "revision": "d44ae48ea0c2364a2dc90603efa7cced",
+    "url": "/static/img/browser@2x.d44ae48e.png"
+  },
+  {
+    "revision": "29d15398068512f22fcaa49087274278",
+    "url": "/static/img/danbao-bg.29d15398.svg"
+  },
+  {
+    "revision": "f62a8ff009c82552a019f799de955cac",
+    "url": "/static/img/default.f62a8ff0.png"
+  },
+  {
+    "revision": "eaaa75d7a9b7566950c439418f7b54d2",
+    "url": "/static/img/drawBg.eaaa75d7.svg"
+  },
+  {
+    "revision": "ecfdc67661b8cdfc4a022ffa5cd3ab5d",
+    "url": "/static/img/empty-noBalance-light.ecfdc676.svg"
+  },
+  {
+    "revision": "c45d86af74a4117196598a7ec02942f5",
+    "url": "/static/img/empty-noData-dark.c45d86af.svg"
+  },
+  {
+    "revision": "9816939cb2282fbf2c068b9a19dffb4f",
+    "url": "/static/img/empty-noData-light.9816939c.svg"
+  },
+  {
+    "revision": "f79c66574cf08d6f342a7a6d3be55553",
+    "url": "/static/img/empty-noSearch-dark.f79c6657.svg"
+  },
+  {
+    "revision": "7e616a93755018473ab4f7610a1e6269",
+    "url": "/static/img/empty-noSearch-light.7e616a93.svg"
+  },
+  {
+    "revision": "0761d8da18eef70350116d41734fbcfc",
+    "url": "/static/img/ftx.0761d8da.png"
+  },
+  {
+    "revision": "36275cfce6fd298004bcc4f1edccf309",
+    "url": "/static/img/gate.36275cfc.png"
+  },
+  {
+    "revision": "291f9b13ab47aab65d3bf41505457664",
+    "url": "/static/img/goPlus-logo.291f9b13.png"
+  },
+  {
+    "revision": "b38daf6f9007d977d84be2e5bd4ae708",
+    "url": "/static/img/huobi.b38daf6f.png"
+  },
+  {
+    "revision": "75f6e9a0019cdd5252d2925b4a9acd52",
+    "url": "/static/img/icon-default.75f6e9a0.png"
+  },
+  {
+    "revision": "4ebadc7855a4dbc4f3f80e5e77643c32",
+    "url": "/static/img/icon-inscription-default.4ebadc78.png"
+  },
+  {
+    "revision": "9509767e940ed68765578589c80f5e0d",
+    "url": "/static/img/iconfont.9509767e.svg"
+  },
+  {
+    "revision": "5d7b85c12e27a0b3f576725c7556d69d",
+    "url": "/static/img/index.5d7b85c1.svg"
+  },
+  {
+    "revision": "d160d9e3bf6187bfe1e6a1a5d5a1e27e",
+    "url": "/static/img/kucoin.d160d9e3.png"
+  },
+  {
+    "revision": "3a7d87c2c63e07a3a297474c83023ae8",
+    "url": "/static/img/logo.3a7d87c2.png"
+  },
+  {
+    "revision": "98b90f1313168d459837a379596d3c69",
+    "url": "/static/img/metamask.98b90f13.svg"
+  },
+  {
+    "revision": "84ed108b5fc6365c0cdda0487e7cb97e",
+    "url": "/static/img/mexc.84ed108b.png"
+  },
+  {
+    "revision": "3c34d7864b5052658b78e49b87494e50",
+    "url": "/static/img/num.3c34d786.svg"
+  },
+  {
+    "revision": "c4b1caf2373b6be184f53bfa0778601c",
+    "url": "/static/img/okx.c4b1caf2.png"
+  },
+  {
+    "revision": "a4b467ff1c22bdb3ca2dc75643224c13",
+    "url": "/static/img/open-en.a4b467ff.png"
+  },
+  {
+    "revision": "9e56c2147a66003724a385146e24743b",
+    "url": "/static/img/open-zh.9e56c214.png"
+  },
+  {
+    "revision": "349b489d99d370e273fb8c6be095e3a7",
+    "url": "/static/img/redPacket.349b489d.svg"
+  },
+  {
+    "revision": "f7c173ee26ad01eeb1d4be9f32c8045f",
+    "url": "/static/img/tokenpocket.f7c173ee.png"
+  },
+  {
+    "revision": "f95f80ba236d1d6305f61c88865f8a39",
+    "url": "/static/img/trustwallet.f95f80ba.png"
+  },
+  {
+    "revision": "45cd5bf45bf872a3b2942bcb2976f65a",
+    "url": "/static/img/tx_chat_cn.45cd5bf4.png"
+  },
+  {
+    "revision": "f7210084f389b425a3a46e515d7701e5",
+    "url": "/static/img/tx_cn.f7210084.png"
+  },
+  {
+    "revision": "afc875e91dbd017eae0bc60a08b0e514",
+    "url": "/static/img/tx_en.afc875e9.png"
+  },
+  {
+    "revision": "349b489d99d370e273fb8c6be095e3a7",
+    "url": "/static/img/wallet.349b489d.svg"
+  },
+  {
+    "revision": "5c83ecd77101b66196d5212937f2329b",
+    "url": "/static/img/watermark.5c83ecd7.png"
+  },
+  {
+    "revision": "f88c135c1d6f3c5672861b1d74b05c39",
+    "url": "/static/img/平均成本.f88c135c.svg"
+  },
+  {
+    "revision": "f64f0105d6fe0a8b4e355f7c95cb8c2a",
+    "url": "/static/img/当前持仓.f64f0105.svg"
+  },
+  {
+    "revision": "1a984f7d1a2605a66b43fb2313c062f5",
+    "url": "/static/img/恭喜.1a984f7d.svg"
+  },
+  {
+    "revision": "6dca0cfce8f5522da173a4586515e5b8",
+    "url": "/static/img/持仓金额.6dca0cfc.svg"
+  },
+  {
+    "revision": "db853751bb59661b559929f4a518903e",
+    "url": "/static/img/盈亏情况.db853751.svg"
+  },
+  {
+    "revision": "842e6a3aa2dda2d9fbfaa25d78692ac2",
+    "url": "/static/img/邀请好友.842e6a3a.svg"
+  },
+  {
+    "revision": "e437a3c554fc95c36bad42728e2b68ad",
+    "url": "/static/img/邀请好友点击链接.e437a3c5.png"
+  },
+  {
+    "revision": "07e48bde07b85d28d4fe",
+    "url": "/static/js/Alert.18eda353.js"
+  },
+  {
+    "revision": "b52b71e51b11566beaf6",
+    "url": "/static/js/Balance.ebb8e967.js"
+  },
+  {
+    "revision": "39bc94d0c895eb0ded08",
+    "url": "/static/js/Bownload.0be140d1.js"
+  },
+  {
+    "revision": "7b790b4558079e14da39",
+    "url": "/static/js/Brc20.f8aad9a4.js"
+  },
+  {
+    "revision": "d978d1aa39d249fae0d7",
+    "url": "/static/js/CVAirdrop.7715132b.js"
+  },
+  {
+    "revision": "3ac38eb5c145ad44d2d2",
+    "url": "/static/js/Chat.d7a20e9d.js"
+  },
+  {
+    "revision": "262e98f07d6b0e263186",
+    "url": "/static/js/ChatBanned.1391d1f5.js"
+  },
+  {
+    "revision": "33c24fe6be133653bbe1",
+    "url": "/static/js/ChatBurn.6c777afd.js"
+  },
+  {
+    "revision": "316a1bed8de36f9aca46",
+    "url": "/static/js/ChatIndex.993e9258.js"
+  },
+  {
+    "revision": "655ec0028b7850610e57",
+    "url": "/static/js/ChatNewPayment.6b4c9eab.js"
+  },
+  {
+    "revision": "793de407545e1771ae57",
+    "url": "/static/js/ChatOfficial.4a0a1c0b.js"
+  },
+  {
+    "revision": "3648799da4fb7f9da9e6",
+    "url": "/static/js/ChatPenalty.b697e6f9.js"
+  },
+  {
+    "revision": "5cad4fca0cc4e39c4cf3",
+    "url": "/static/js/ChatSet.ff474fec.js"
+  },
+  {
+    "revision": "8f9ca040af07a3276692",
+    "url": "/static/js/ChatTop.79a99349.js"
+  },
+  {
+    "revision": "dd923aee734b2012ce61",
+    "url": "/static/js/Check.a92cc2f1.js"
+  },
+  {
+    "revision": "3d9321b23ef981a38780",
+    "url": "/static/js/Evm.322c8ca6.js"
+  },
+  {
+    "revision": "1df9649f674596c03829",
+    "url": "/static/js/Fav.4ed3f63d.js"
+  },
+  {
+    "revision": "93138e729fdd36831deb",
+    "url": "/static/js/FavoriteManage.8623220e.js"
+  },
+  {
+    "revision": "e91dfcfaf0796bc8608c",
+    "url": "/static/js/Gas.49b60d4e.js"
+  },
+  {
+    "revision": "e70fd56b9f864cd8d938",
+    "url": "/static/js/Home.306f720e.js"
+  },
+  {
+    "revision": "611b850ca3689289dbcf",
+    "url": "/static/js/Inscription.ab8b09c6.js"
+  },
+  {
+    "revision": "bf54b9f57e6b9deb0841",
+    "url": "/static/js/Invitation.b57b834d.js"
+  },
+  {
+    "revision": "867967411fb65a9c0cec",
+    "url": "/static/js/Login.d980351e.js"
+  },
+  {
+    "revision": "fd6ec276651a3539333b",
+    "url": "/static/js/Lost.22652694.js"
+  },
+  {
+    "revision": "b9ae688bdf735c50f5bd",
+    "url": "/static/js/Market.fa9970b4.js"
+  },
+  {
+    "revision": "98cb561f54393446bcac",
+    "url": "/static/js/Me.8e76b2cb.js"
+  },
+  {
+    "revision": "df1da639c559c2f919df",
+    "url": "/static/js/MyChats.ebec9a2d.js"
+  },
+  {
+    "revision": "3aa6b54832b1ca328c44",
+    "url": "/static/js/NFT.9b5336a8.js"
+  },
+  {
+    "revision": "e022b527b0eac87a7463",
+    "url": "/static/js/Notice.9e1fe064.js"
+  },
+  {
+    "revision": "9bda8d1587288116e535",
+    "url": "/static/js/OfficialVerification.05188a96.js"
+  },
+  {
+    "revision": "bd575f00fb25a04458e4",
+    "url": "/static/js/Plate.7fc62cc4.js"
+  },
+  {
+    "revision": "feb1b85e5fb3677a3e1f",
+    "url": "/static/js/Pool.97d9a09b.js"
+  },
+  {
+    "revision": "9d1ebffcddfca75403f8",
+    "url": "/static/js/RedCreate.9b08813f.js"
+  },
+  {
+    "revision": "e5aec7338f21f54ad68e",
+    "url": "/static/js/RedRecord.d655771c.js"
+  },
+  {
+    "revision": "5a072a5544047d4b5f6f",
+    "url": "/static/js/ShareLink.d27c64ba.js"
+  },
+  {
+    "revision": "3f4088c1329afbab1188",
+    "url": "/static/js/Token.3ef3a84b.js"
+  },
+  {
+    "revision": "e84ce09d4cbdc4ed824a",
+    "url": "/static/js/UserInfo.87a15d90.js"
+  },
+  {
+    "revision": "fb7ca84cc0baa736724b",
+    "url": "/static/js/WhaleTracking.c83aef72.js"
+  },
+  {
+    "revision": "d6ec1c96d896ebc4cb4a",
+    "url": "/static/js/app.b26ea064.js"
+  },
+  {
+    "revision": "291356dac188607c1470",
+    "url": "/static/js/chunk-00eabf7e.932a2c66.js"
+  },
+  {
+    "revision": "33ff4ae88ea120fd76d5",
+    "url": "/static/js/chunk-3884f580.e54e15ae.js"
+  },
+  {
+    "revision": "9e9f5172764abdd0c77a",
+    "url": "/static/js/chunk-490414b1.45d5afa4.js"
+  },
+  {
+    "revision": "c11f0af6fae78b75334d",
+    "url": "/static/js/chunk-78b1fbf1.d2dde14e.js"
+  },
+  {
+    "revision": "a1a3687a4f45854767ca",
+    "url": "/static/js/chunk-a0ce77a4.833e725e.js"
+  },
+  {
+    "revision": "0d5ae7fff73b2b4dcbe2",
+    "url": "/static/js/chunk-commons.2e744f5f.js"
+  },
+  {
+    "revision": "99c1d1134f011eb3ade5",
+    "url": "/static/js/chunk-echarts.c7877cab.js"
+  },
+  {
+    "revision": "0abf3bdbf2b76ab8d0bd",
+    "url": "/static/js/chunk-libs.d320a2ca.js"
+  },
+  {
+    "revision": "d123d9b2bc87788efb2f",
+    "url": "/static/js/chunk-vant.38c2baec.js"
+  },
+  {
+    "revision": "7d20b38db7d9cb0b7367",
+    "url": "/static/js/runtime.c13a833b.js"
+  },
+  {
+    "revision": "092cbdafbaa5d88adbad",
+    "url": "/static/js/vendors~Brc20~Chat~ChatTop~NFT~Token.86e61a06.js"
+  },
+  {
+    "revision": "aae84b07c8e98e894e02",
+    "url": "/static/js/vendors~Brc20~NFT.d450b5fa.js"
+  },
+  {
+    "revision": "4a5e95145eadda96c091",
+    "url": "/static/js/vendors~ChatPenalty.46e8960b.js"
+  },
+  {
+    "revision": "5523f931fd9f39f41220",
+    "url": "/static/js/vendors~ChatSet~UserInfo.1673f313.js"
+  },
+  {
+    "revision": "197f5743c21a1e6c4f1f",
+    "url": "/static/js/vendors~Chat~ChatTop.26e3603d.js"
+  },
+  {
+    "revision": "68d3bddaab2fe77672e5",
+    "url": "/static/js/vendors~Gas~Market.0277e7ab.js"
+  },
+  {
+    "revision": "e8f183932b8ba65ebf58",
+    "url": "/static/js/vendors~Home.88fcf7f6.js"
+  },
+  {
+    "revision": "87d984aea7ac9a39632f",
+    "url": "/static/js/vendors~Home~Token~WhaleTracking.ad456d37.js"
+  },
+  {
+    "revision": "d60d3442464046a59a2f",
+    "url": "/static/js/vendors~Market.173592d0.js"
+  }
+]);
